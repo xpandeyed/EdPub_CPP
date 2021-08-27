28eef561c372f4837b77e1384a24f285605e1813
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2021-08-27 17:03:28 +0530

    App bar functioning fine, hurray

--- app/src/main/java/com/edpub/cpp/HomeActivity.kt
+++ app/src/main/java/com/edpub/cpp/HomeActivity.kt
@@ -1,7 +1,11 @@
 package com.edpub.cpp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -9,6 +13,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(findViewById(R.id.tbHomeToolBar))
 
         val homeFragment = HomeFragment()
         val chaptersFragment = ChaptersFragment()
@@ -33,6 +38,20 @@ class HomeActivity : AppCompatActivity() {
             replace(R.id.flFragmentContainer, fragment)
             commit()
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_activity_appbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miProfile->{
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return true
     }
 
 }
