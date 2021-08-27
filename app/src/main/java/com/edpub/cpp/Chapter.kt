7d24cacf02c80e60f1968097f8df1e863c484d9e
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2021-08-27 21:02:36 +0530

    Recycler View data is loaded from database for chapeters

--- app/src/main/java/com/edpub/cpp/Chapter.kt
+++ app/src/main/java/com/edpub/cpp/Chapter.kt
@@ -1,3 +1,3 @@
 package com.edpub.cpp
 
-data class Chapter(var KEY:String,var INDEX:String, var TITLE:String,var TEXT:String,var CODE:String)
+data class Chapter(var KEY:String?=null,var INDEX:String?=null, var TITLE:String?=null,var TEXT:String?=null,var CODE:String?=null)
--- app/src/main/java/com/edpub/cpp/LauncherActivity.kt
+++ app/src/main/java/com/edpub/cpp/LauncherActivity.kt
@@ -6,12 +6,28 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+
+        val databaseReference : DatabaseReference = FirebaseDatabase.getInstance().getReference("CHAPTERS")
+        databaseReference.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(chapter in snapshot.children){
+                        val currChapter = chapter.getValue(Chapter::class.java)
+                        ObjectsCollection.chaptersList.add(currChapter!!)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@LauncherActivity, "$error", Toast.LENGTH_SHORT).show()
+            }
+        })
 
         val auth: FirebaseAuth = Firebase.auth
         if(auth.currentUser==null)
