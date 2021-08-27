7bccb344e5275ff3fa9f7a5b18b758030a08d024
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2021-08-27 22:27:48 +0530

    Everything working fine, data is not loaded in background

--- app/src/main/java/com/edpub/cpp/LauncherActivity.kt
+++ app/src/main/java/com/edpub/cpp/LauncherActivity.kt
@@ -8,37 +8,49 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
 
-        val databaseReference : DatabaseReference = FirebaseDatabase.getInstance().getReference("CHAPTERS")
-        databaseReference.addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists()){
-                    for(chapter in snapshot.children){
-                        val currChapter = chapter.getValue(Chapter::class.java)
-                        ObjectsCollection.chaptersList.add(currChapter!!)
-                    }
-                }
-            }
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@LauncherActivity, "$error", Toast.LENGTH_SHORT).show()
-            }
-        })
 
-        val auth: FirebaseAuth = Firebase.auth
-        if(auth.currentUser==null)
-        {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+        CoroutineScope(Dispatchers.IO).launch {
+            async{
+                val databaseReference : DatabaseReference = FirebaseDatabase.getInstance().getReference("CHAPTERS")
+                databaseReference.addValueEventListener(object: ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if(snapshot.exists()){
+                            for(chapter in snapshot.children){
+                                val currChapter = chapter.getValue(Chapter::class.java)
+                                ObjectsCollection.chaptersList.add(currChapter!!)
+                            }
+                        }
+                    }
+                    override fun onCancelled(error: DatabaseError) {
+                        Toast.makeText(this@LauncherActivity, "$error", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }.await()
+            val auth: FirebaseAuth = Firebase.auth
+            if(auth.currentUser==null)
+            {
+                val intent = Intent(this@LauncherActivity, SignUpActivity::class.java)
+                startActivity(intent)
+            }
+            else
+            {
+                val intent = Intent(this@LauncherActivity, HomeActivity::class.java)
+                startActivity(intent)
+            }
         }
-        else
-        {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
+
+
+
+
     }
 }
--- app/src/main/java/com/edpub/cpp/ProfileActivity.kt
+++ app/src/main/java/com/edpub/cpp/ProfileActivity.kt
@@ -2,10 +2,13 @@ package com.edpub.cpp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        setSupportActionBar(findViewById(R.id.tbProfileToolBar))
+        findViewById<androidx.appcompat.widget.Toolbar>(R.id.tbProfileToolBar)
     }
 }
