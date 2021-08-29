9604abf7b3ec0f8c700cd45e8f3ca51c91469183
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2021-08-29 22:01:50 +0530

    Account deletion functionality added successfully

--- app/src/main/java/com/edpub/cpp/UpdateProfileActivity.kt
+++ app/src/main/java/com/edpub/cpp/UpdateProfileActivity.kt
@@ -1,10 +1,16 @@
 package com.edpub.cpp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UpdateProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +23,17 @@ class UpdateProfileActivity : AppCompatActivity() {
             .setTitle("Are you sure you want to delete your account?")
             .setMessage("Deleting your account will delete all of your data stored with us.")
             .setPositiveButton("Delete Account") { _, _ ->
-                Toast.makeText(this, "We are sad to see you going.", Toast.LENGTH_SHORT).show()
+                val deleteUser = Firebase.auth.currentUser!!
+                CoroutineScope(Dispatchers.IO).launch {
+                    deleteUser.delete()
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this@UpdateProfileActivity, "We are sad to see you going.", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this@UpdateProfileActivity, SignUpActivity::class.java)
+                                startActivity(intent)
+                            }
+                        }
+                }
             }
             .setNegativeButton("Cancel") { _, _ ->
                 Toast.makeText(this, "Account deletion cancelled.", Toast.LENGTH_SHORT).show()
@@ -31,6 +47,7 @@ class UpdateProfileActivity : AppCompatActivity() {
 
         }
         findViewById<Button>(R.id.bToDeleteAccount).setOnClickListener {
+            Toast.makeText(this, "Make sure you have logged in recently. If not so, please logout and login once.", Toast.LENGTH_LONG).show()
             deleteAlertDialogBox.show()
         }
     }
