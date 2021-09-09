e3ca74e0f82700cb322a262993d39861420a3934
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2021-09-09 15:02:06 +0530

    Favourites are fucked up.

--- app/src/main/java/com/edpub/cpp/FunctionCollection.kt
+++ app/src/main/java/com/edpub/cpp/FunctionCollection.kt
@@ -1,7 +1,6 @@
 package com.edpub.cpp
 
-import android.content.Context
-import android.widget.Toast
+import android.util.Log
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
@@ -9,11 +8,12 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.*
 
 object FunctionCollection {
-    suspend fun copyFavouriteChapters () {
-
+    fun copyFavouriteChapters () {
+        Log.i("FAV", "Copying Favourite Chapters")
         ObjectsCollection.favouriteChapters.clear()
-        val job = CoroutineScope(Dispatchers.Main).async {
+        CoroutineScope(Dispatchers.Main).launch {
             var n = 0
+            Log.i("FAV", "Size of chapter list : ${ObjectsCollection.chaptersList.size}")
             while (n < ObjectsCollection.chaptersList.size) {
                 var index = ObjectsCollection.favouriteChapterKeysList.indexOf(ObjectsCollection.chaptersList[n].KEY)
                 if (index != -1) {
@@ -21,14 +21,14 @@ object FunctionCollection {
                     ObjectsCollection.adapterFavouriteChapters.notifyItemInserted(ObjectsCollection.favouriteChapters.size-1)
                 }
                 n++
+                Log.i("FAV", "$n = ${ObjectsCollection.favouriteChapters}")
             }
             ObjectsCollection.areFavouriteChaptersCopied = true
         }
-        job.await()
 
     }
     suspend fun loadChapters () {
-
+        Log.i("FAV", "Loading chapters...")
         val job = CoroutineScope(Dispatchers.IO).async {
             val databaseReference : DatabaseReference = FirebaseDatabase.getInstance().getReference("CHAPTERS")
             databaseReference.addValueEventListener(object: ValueEventListener {
@@ -37,6 +37,7 @@ object FunctionCollection {
                         for(chapter in snapshot.children){
                             val currChapter = chapter.getValue(Chapter::class.java)
                             ObjectsCollection.chaptersList.add(currChapter!!)
+                            Log.i("FAV", "${ObjectsCollection.chaptersList}")
                             ObjectsCollection.adapterChapters.notifyItemInserted(ObjectsCollection.chaptersList.size-1)
                         }
                     }
@@ -46,15 +47,16 @@ object FunctionCollection {
                     ObjectsCollection.isDataLoaded= false
                 }
             })
+
         }
-        job.await()
-        if(!ObjectsCollection.isNewUser){
+        job.join()
+        if(!ObjectsCollection.isNewUser && job.isCompleted){
             loadFavouriteChapterKeys()
         }
     }
     private suspend fun loadFavouriteChapterKeys (){
-
-        val job = CoroutineScope(Dispatchers.IO).async {
+        Log.i("FAV", "Loading fav chapters")
+        CoroutineScope(Dispatchers.IO).launch{
             val favChapterReference = Firebase.database.getReference("USERS").child(Firebase.auth.currentUser!!.uid).child("FAV_CHAP")
             favChapterReference.addValueEventListener(object: ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
@@ -62,6 +64,7 @@ object FunctionCollection {
                         for(chapter in snapshot.children){
                             val currChapter = chapter.getValue(String::class.java)
                             ObjectsCollection.favouriteChapterKeysList.add(currChapter!!)
+                            Log.i("FAV", "${ObjectsCollection.favouriteChapterKeysList}")
                         }
                     }
                     ObjectsCollection.isFavouriteChapterKeysListLoaded = true
@@ -71,9 +74,8 @@ object FunctionCollection {
                 }
             })
         }
-        job.await()
 
-        copyFavouriteChapters()
+
     }
 
 }
