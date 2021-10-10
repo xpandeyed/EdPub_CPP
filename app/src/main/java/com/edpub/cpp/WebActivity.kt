680b900f6acfdb60a0eeb47fa2bcc52024f21103
lalbiharipandeyg@gmail.com <lalbiharipandeyg@gmail.com>
2021-10-10 08:57:35 +0530

    Progress bar added for webview.

--- app/src/main/java/com/edpub/cpp/WebActivity.kt
+++ app/src/main/java/com/edpub/cpp/WebActivity.kt
@@ -2,18 +2,30 @@ package com.edpub.cpp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
 
-        val url = intent.getStringExtra("URL")
         val wbMyWebView = findViewById<WebView>(R.id.wbMyWebView)
-        wbMyWebView.webViewClient = WebViewClient()
-        wbMyWebView.getSettings().setJavaScriptEnabled(true)
+        val pbWebPageProgress = findViewById<CircularProgressIndicator>(R.id.pbWebPageProgress)
+
+        val url = intent.getStringExtra("URL")
+        if(url!=null){
+            wbMyWebView.settings.javaScriptEnabled = true
+            wbMyWebView.webViewClient = object : WebViewClient(){
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    wbMyWebView.visibility = View.VISIBLE
+                    pbWebPageProgress.visibility = View.GONE
+                }
+            }
+        }
         wbMyWebView.loadUrl(url!!)
     }
 }
