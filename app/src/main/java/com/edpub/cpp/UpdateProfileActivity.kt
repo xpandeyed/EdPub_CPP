ef85b806b197d86dbf3ac146472d072b882cbfb7
lalbiharipandeyg@gmail.com <lalbiharipandeyg@gmail.com>
2021-10-26 13:11:24 +0530

    Layouts changed.

--- app/src/main/java/com/edpub/cpp/UpdateProfileActivity.kt
+++ app/src/main/java/com/edpub/cpp/UpdateProfileActivity.kt
@@ -13,15 +13,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthCredential
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
